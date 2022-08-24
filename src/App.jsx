d0c9463f402756a93b22bf1f7f1d98077cfa0a5b
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2022-08-24 11:24:00 +0000

    Created a simple UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+const App = () => {
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>
-        Click on the Tauri, Vite, and React logos to learn more about each
-        framework.
-      </p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+    <div className="min-h-screen min-w-screen flex justify-center text-center ">
+      <div className="flex justify-center flex-col gap-5 items-center w-2/3 ">
+      <h1 className="text-5xl mb-3 font-bold">Hopel.io</h1>
+        <textarea className="w-5/6 h-2/4 p-2 rounded-md shadow-md" placeholder="Input Text"/>
+        <div className="grid grid-cols-2 gap-y-5 gap-x-3 items-center">
+          <h1>Number of Columns:</h1>
+          <input type="number" defaultValue={2} />
+          <h1>Number of words per column:</h1>
+          <input type="number" defaultValue={2}/>
         </div>
+        <button>Create your Hopel</button>
       </div>
-
-      <p>{greetMsg}</p>
     </div>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Counter from "./counter";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Counter />
+    {/* <Counter /> */}
   </React.StrictMode>
 );
